8c49e4e11f40080181b97453b4126721f4de34e8
sofiaCoder <sofia.laas@gmail.com>
2024-12-17 14:03:30 +0100

    Gallery started up

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -1,5 +1,22 @@
+import Image from 'next/image';
+
+const images = [
+  '/images/notesHeart.jpg',
+  '/images/choir2.jpg',
+  '/images/notes.jpg',
+];
+
 const Gallery = () => {
-  return <section id='gallery'>Gallery</section>;
+  return (
+    <section id='gallery'>
+      <h2>Gallery</h2>
+      <div className='my-10 flex gap-5'>
+        {images.map((image, index) => (
+          <Image key={index} src={image} alt='notes' width={400} height={400} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Gallery;
